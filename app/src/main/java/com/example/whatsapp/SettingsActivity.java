55ca3615dbce58e7a93e83958d5ec16f5577d21c
SaiVenkat168 <saivenkatsalpala@gmail.com>
2022-01-29 08:35:52 +0530

    whatsapp clone

---
+++ app/src/main/java/com/example/whatsapp/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.whatsapp.databinding.ActivitySettingsBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this,MainActivity.class));
+            }
+        });
+
+    }
+}
